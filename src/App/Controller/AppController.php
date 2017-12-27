1b38f41f366ef2ee6fbbe6550018f6699c92a98e
Petru Binzari <petru_binzari@yahoo.com>
2017-12-27 12:58:00 +0200

    parameters

--- src/App/Controller/AppController.php
+++ src/App/Controller/AppController.php
@@ -6,10 +6,14 @@ use App\Entity\Config;
 use BotMan\BotMan\BotMan;
 use BotMan\BotMan\BotManFactory;
 use BotMan\BotMan\Drivers\DriverManager;
+use BotMan\Drivers\Facebook\Extensions\Element;
+use BotMan\Drivers\Facebook\Extensions\ElementButton;
+use BotMan\Drivers\Facebook\Extensions\GenericTemplate;
 use BotMan\Drivers\Facebook\FacebookDriver;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
@@ -168,8 +172,20 @@ class AppController extends Controller
                 'client_id' => $this->getParameter('fb.app_id'),
                 'client_secret' => $this->getParameter('fb.app_secret'),
                 'grant_type' => 'client_credentials',
-                'scope' => 'pages_messaging, pages_messaging_phone_number, pages_messaging_subscriptions'
+                'scope' => 'pages_messaging, pages_messaging_phone_number, pages_messaging_subscriptions,
+                            messages, messaging_postbacks, messaging_optins, message_deliveries, message_reads, 
+                            messaging_payments, messaging_pre_checkouts, messaging_checkout_updates, messaging_account_linking, 
+                            messaging_referrals, message_echoes, standby, messaging_handovers, messaging_policy_enforcement'
             ];
+            //messages, messaging_postbacks, messaging_optins, message_deliveries, message_reads,
+            // messaging_payments, messaging_pre_checkouts, messaging_checkout_updates,
+            // messaging_account_linking, messaging_referrals, message_echoes, standby, messaging_handovers, messaging_policy_enforcement
+
+            //https://www.facebook.com/v2.6/dialog/oauth?response_type=token&display=popup&client_id=350300872106773&redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fexplorer%2Fcallback&scope=
+            //manage_pages
+            //pages_messaging
+            //pages_messaging_phone_number
+            //Cpages_messaging_subscriptions
 
             $us = $fb->get('/oauth/access_token?' . http_build_query($params));
             $us = $us->getGraphNode()->asArray();
@@ -210,12 +226,12 @@ class AppController extends Controller
     {
         $config = [
             'facebook' => [
-                'token' => 'EAAEZBmMcL4xUBALKtcficMH05A87inhoWQyHEFYftftA6A4b8zi1ni5BRnp3bCgU5PDAIwucPOKuDqYEXpK1f5Yv6GDBb7Fgz7yudZAGZCF7CBnAlbGMtaG5Mhr8wZBJ5mBryyppMtiYAIOMgV2AekQ90ZAWc7EZCrWijPYZCrVfAZDZD',
+                'token' => 'EAAEZBmMcL4xUBAF2A1LcBZAYj3gN4dL8Eq596rtDkcGb6ZAB3SCBZCmqW52sq9axnZAtZATqhfwcg7MJgZBrjrvVdBqnp3FyQzdMoRZCDdYlNpaC8XZCY2oa1wyH4K4d5nVC9GnCiRqhpLUbkJGqVeJZBRnWyI6QaZCzJhaUX63Nnv71AZDZD',
                 'app_secret' => $this->getParameter('fb.app_secret'),
                 'verification'=>'test',
             ]];
 
-        $config['facebook']['token'] = "EAAEZBmMcL4xUBAFSHptCeALMBoHvmXXC7kH2g0K7KCnt4a8KZAAZBpyuLqBBKExX8854cMsKdZCvsIZBMZCE9TpgsaIshz9ndNV1ZA3p88hSorF3gjjrI1ItcEceHd2APxUKnu54hRwbihv44SKhZC5K1J5WuBnyYNBhFgsfW9YQwQZDZD";
+        $config['facebook']['token'] = "EAAEZBmMcL4xUBAF2A1LcBZAYj3gN4dL8Eq596rtDkcGb6ZAB3SCBZCmqW52sq9axnZAtZATqhfwcg7MJgZBrjrvVdBqnp3FyQzdMoRZCDdYlNpaC8XZCY2oa1wyH4K4d5nVC9GnCiRqhpLUbkJGqVeJZBRnWyI6QaZCzJhaUX63Nnv71AZDZD";
 
         DriverManager::loadDriver(FacebookDriver::class);
 
@@ -225,9 +241,98 @@ class AppController extends Controller
             $bot->reply('Hello World');
         });
 
+        $botman->hears('noroc', function (BotMan $bot) {
+            $bot->reply('norocel');
+        });
+
+        $botman->hears('{text}', function (BotMan $bot, $text) {
+            $bot->reply($text);
+        });
+
+        $botman->on('messaging_optins', function($payload, BotMan $bot) {
+
+            $ad = $payload;
+            $bot->say(
+                GenericTemplate::create()
+                    ->addImageAspectRatio(GenericTemplate::RATIO_SQUARE)
+                    ->addElements([
+                        Element::create('BotMan Documentation')
+                            ->subtitle('All about BotMan')
+                            ->image('http://botman.io/img/botman-body.png')
+                            ->addButton(ElementButton::create('visit')->url('http://botman.io'))
+                            ->addButton(ElementButton::create('tell me more')
+                                ->payload('tellmemore')->type('postback')),
+                        Element::create('BotMan Laravel Starter')
+                            ->subtitle('This is the best way to start with Laravel and BotMan')
+                            ->image('http://botman.io/img/botman-body.png')
+                            ->addButton(ElementButton::create('visit')
+                                ->url('https://github.com/mpociot/botman-laravel-starter')
+                            )
+                    ]),
+                '1450167398384668',
+                FacebookDriver::class
+            );
+        });
+
+        $botman->on('messaging_referrals', function($payload, BotMan $bot) {
+
+            $bot->say('welcome', $payload);
+        });
+
+        $botman->on('messaging_optins', function($payload, BotMan $bot) {
+            $bot->say('welcome', $payload);
+        });
+
         $botman->listen();
 
         return new Response();
+    }
+
+    /**
+     * @Route("/send", name="app_send")
+     */
+    public function sendAction(Request $request)
+    {
+        $config = [
+            'facebook' => [
+                'token' => 'EAAEZBmMcL4xUBAJU7cHTVz3f1a5btlwGzT7p10dgdjbeFTqe770tfZA49rHvrjEEcHkTDfgPmQfkaV9wrvlDpqfhvZCmN2EMUYl6ZCu94vqIQuZB0YzQcnHpIyfD7h8HF55PQat9bvSotDEXjeZBlr3QqZBg1UDw2f5bCjMnZC8ZBYAZDZD',
+                'app_secret' => $this->getParameter('fb.app_secret'),
+                'verification'=>'test',
+            ]];
+
+        DriverManager::loadDriver(FacebookDriver::class);
+
+        $botman = BotManFactory::create($config);
+
+//        $botman->on('messaging_optins', function($payload, BotMan $botman) {
+        $botman->say(
+                GenericTemplate::create()
+                    ->addImageAspectRatio(GenericTemplate::RATIO_SQUARE)
+                    ->addElements([
+                        Element::create('BotMan Documentation')
+                            ->subtitle('All about BotMan')
+                            ->image('http://botman.io/img/botman-body.png')
+                            ->addButton(ElementButton::create('visit')->url('http://botman.io'))
+                            ->addButton(ElementButton::create('tell me more')
+                                ->payload('tellmemore')->type('postback')),
+                        Element::create('BotMan Laravel Starter')
+                            ->subtitle('This is the best way to start with Laravel and BotMan')
+                            ->image('http://botman.io/img/botman-body.png')
+                            ->addButton(ElementButton::create('visit')
+                                ->url('https://github.com/mpociot/botman-laravel-starter')
+                            )
+                    ]),
+                '1642945535776920',
+                FacebookDriver::class
+            );
+//        });
+
+
+
+        $botman->listen();
+
+
+        return new JsonResponse(true);
     }
 
     /**
@@ -237,4 +342,22 @@ class AppController extends Controller
     {
         return $this->render('pixel.html.twig', []);
     }
+
+    /**
+     * @Route("/checkbox", name="app_checkbox")
+     */
+    public function checkboxAction(Request $request)
+    {
+        return $this->render('checkbox.html.twig', [
+            'origin' => $this->generateUrl('app_webhook', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'appId' => $this->getParameter('fb.app_id'),
+            'pageId' => '125784381475358 ',
+            'uniqueId' => rand(100000, 999999),
+        ]);
+    }
 }
+//https://www.facebook.com/tr/?id=350300872106773&
+//ev=MessengerCheckboxUserConfirmation&
+//dl=https%3A%2F%2Fb4c10fb5.ngrok.io%2Fcheckbox&
+//rl=&if=false&ts=1512650733262&cd%5Bapp_id%5D=350300872106773&cd%5Bpage_id%5D=125784381475358%20&cd%5B
+//ref%5D=webhook&cd%5Buser_ref%5D=329864&sw=1920&sh=1080&dt=4xe3gcu2rh8apqgzerhbmtnqrbf4j1tp
